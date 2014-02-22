97460c98cf4b431c935f6f0367e7dee743e98672
Discoverivan <pritychenko-ivan@yandex.ru>
2014-02-22 15:02:37 +0400

    41
    Удалить из строки с n по m символы

---
+++ S02/41/41/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _41
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s,n,m;
+            int i,k;
+            //reading
+            s = Console.ReadLine();
+            n = Console.ReadLine();
+            m = Console.ReadLine();
+            //code
+            k = Convert.ToInt32(m) - Convert.ToInt32(n);
+            s=s.Remove(Convert.ToInt32(n), k);
+            Console.WriteLine(s);
+        }
+    }
+}
