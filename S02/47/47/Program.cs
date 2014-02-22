f9466d3cc47262e6a43d9f630ea5f8979dc706c4
Discoverivan <pritychenko-ivan@yandex.ru>
2014-02-22 15:16:20 +0400

    47
    Вставить заданный символ после первой буквы и

---
+++ S02/47/47/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _47
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s, z;
+            int i;
+
+            s = Console.ReadLine();
+            z = Console.ReadLine();
+
+            for (i = 0; i < s.Length; i++)
+            {
+                if (s[i].ToString() == "и")
+                {
+                    s = s.Insert(i +1, z);
+                    break;
+                }
+            }
+            Console.WriteLine(s);
+        }
+    }
+}
