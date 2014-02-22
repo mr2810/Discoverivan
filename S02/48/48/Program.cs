9a21deb2742d576bdd2930c5f2843f2c99666ff8
Discoverivan <pritychenko-ivan@yandex.ru>
2014-02-22 15:10:19 +0400

    48
    Вставить заданный символ после последней буквы и

---
+++ S02/48/48/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _48
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s,z;
+            int i;
+
+            s = Console.ReadLine();
+            z = Console.ReadLine();
+
+            for (i = s.Length-1; i > 0; i--) 
+            {
+                if (s[i].ToString() == "и") 
+                {
+                    s = s.Insert(i + 1, z);
+                }
+            }
+            Console.WriteLine(s);
+
+        }
+    }
+}
