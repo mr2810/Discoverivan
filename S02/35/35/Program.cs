7193e202c0f9b98445e46e31236b707e6c2100cd
Discoverivan <pritychenko-ivan@yandex.ru>
2014-02-22 14:45:05 +0400

    35
    Удалить символы стоящие на четных позициях.

---
+++ S02/35/35/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _35
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            string s,sb;
+            int i;
+            sb = "";
+            s=Console.ReadLine();
+            for (i = 0; i < s.Length;i++)
+            {
+                if (i % 2 == 0) 
+                {
+                    sb = sb + s[i];
+                }
+            }
+            Console.WriteLine(sb);
+            Console.ReadLine();
+        }
+    }
+}
